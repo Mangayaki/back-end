bc23b9d96786973f291cb894c7f258e018b24435
Matty Zappy <119907550+MattyGaryuu@users.noreply.github.com>
2023-05-23 15:59:00 -0300

    integrando modelos a database

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    ID = models.AutoField(primary_key=True)
+    usuario =  models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    senha1 = models.CharField(max_length=30)
+    senha2 = models.CharField(max_length=30)
+    pass
+
+class UserFav(models.Model):
+    usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    id_manga = models.IntegerField()
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -89,6 +89,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'authentication.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
